87318ed0c53493ae97e1b8e981765dad8fbaa22a
Kieran <mcclymont.kieran@gmail.com>
2018-05-25 15:00:40 -0300

    Added a build section with pictures of the various forest layers

---
+++ src/Components/Build.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import '../CSS/Build.css'
+
+const floor = require('../Images/floor.jpg')
+const underStory = require('../Images/understory.jpg')
+const canopy = require('../Images/canopy.jpg')
+const emergent = require('../Images/emergent.jpg')
+
+class Build extends Component {
+  state = {
+    expandedBox: false
+  }
+
+  expandInfo(e) {
+    const currentTarget = e.target.id
+    console.log(e.target.nextElementSibling.style.maxHeight)
+    e.target.nextElementSibling.style.maxHeight === '4000px'
+    ? e.target.nextElementSibling.style.maxHeight = null
+    : e.target.nextElementSibling.style.maxHeight = '4000px'
+    const container = document.getElementsByClassName('buildContainer')[0]
+    let selector = 'expanded' + currentTarget
+    container.classList.toggle(selector)
+  }
+
+  render() {
+    return(
+      <div className='buildContainer'>
+        <div className='forestInfoBox level1'>
+          <h1 className='titleButton' id='Level1' onClick={ e => this.expandInfo(e)}>Emergent Layer</h1>
+          <div className='hiddenInformation'>
+            <img src={emergent} className='forestPic'/>
+          </div>
+        </div>
+        <div className='forestInfoBox level2'>
+          <h1 className='titleButton' id='Level2' onClick={ e => this.expandInfo(e)}>Canopy Layer</h1>
+          <div className='hiddenInformation'>
+            <img src={canopy} className='forestPic'/>
+          </div>
+        </div>
+        <div className='forestInfoBox level3'>
+          <h1 className='titleButton' id='Level3' onClick={ e => this.expandInfo(e)}>Understory</h1>
+          <div className='hiddenInformation'>
+            <img src={underStory} className='forestPic'/>
+          </div>
+        </div>
+        <div className='forestInfoBox level4'>
+          <h1 className='titleButton' id='Level4' onClick={ e => this.expandInfo(e)}>Forest Floor</h1>
+          <div className='hiddenInformation'>
+            <img src={floor} className='forestPic'/>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Build
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,6 +3,7 @@ import { Route, HashRouter as Router } from 'react-router-dom'
 import Banner from './Banner'
 import Home from './Home'
 import Donate from './Donate'
+import Build from './Build'
 
 class Main extends Component {
 
@@ -13,6 +14,7 @@ class Main extends Component {
           <Route render={(props) => <Banner {...this.props} {...props}/>} />
           <Route exact path='/' render={(props) => <Home {...this.props} {...props}/>}/>
           <Route exact path='/donate' render={(props) => <Donate {...this.props} />}/>
+          <Route exact path='/build' render={props => <Build {...this.props}/>} />
         </div>
       </Router>
     )
